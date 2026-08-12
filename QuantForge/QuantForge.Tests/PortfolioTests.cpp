89a0820a861acc0286b7445bc8e0449dea751d02
Nassim Sahib <nassim.sahib775@gmail.com>
2026-08-12 16:32:09 +0200

    feat(portfolio): add Portfolio position container

---
+++ QuantForge/QuantForge.Tests/PortfolioTests.cpp
@@ -0,0 +1,50 @@
+#include "pch.h"
+
+#include <utility>
+
+#include "Portfolio.h"
+#include "OptionType.h"
+#include "EuropeanOption.h"
+
+namespace quantforge::portfolio {
+	class PortfolioTests : public ::testing::Test {
+	protected:
+		std::shared_ptr<const quantforge::instruments::EuropeanOption> callOption = 
+			std::make_shared< quantforge::instruments::EuropeanOption>(
+				"OPT_AAPL_001", 
+				1.0, 
+				100.0, 
+				quantforge::instruments::OptionType::Call);
+		std::shared_ptr<const quantforge::instruments::EuropeanOption> putOption=
+			std::make_shared<quantforge::instruments::EuropeanOption>(
+				"OPT_AAPL_002",
+				1.0,
+				100.0,
+				quantforge::instruments::OptionType::Put);
+			
+		Position pos1{ callOption, 5 };
+		Position pos2{ putOption, 20};
+	};
+
+	TEST_F(PortfolioTests, NewPortfolioIsEmpty) {
+		Portfolio portfolio;
+		EXPECT_EQ(portfolio.empty(), true);
+	}
+	TEST_F(PortfolioTests, AddPositionIncreaseSize) {
+		Portfolio portfolio;
+		portfolio.addPosition(pos1);
+		EXPECT_EQ(portfolio.size(), 1);
+	}
+	TEST_F(PortfolioTests, StoresAddedPositions) {
+		Portfolio portfolio;
+		portfolio.addPosition(pos1);
+		portfolio.addPosition(pos2);
+		EXPECT_EQ(portfolio.size(), 2);
+
+		EXPECT_EQ(portfolio.positions()[0].quantity(), 5);
+		EXPECT_EQ(portfolio.positions()[1].quantity(), 20);
+
+		EXPECT_EQ(&portfolio.positions()[0].instrument(), callOption.get());
+		EXPECT_EQ(&portfolio.positions()[1].instrument(), putOption.get());
+	}
+}
